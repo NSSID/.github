7364c54595254abcfcb945527d0abb4c271b1edc
CupMenolakMC <151956012+Yusupkakuu@users.noreply.github.com>
2023-11-25 00:39:33 +0700

    Create Backup ke 2.py

---
+++ profile/Backup ke 2.py
@@ -0,0 +1,26 @@
+[![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#table-of-contents)
+
+# `Account information`
+> we only copy or fork the repo repo on github, we don't consider it completely ours, we also still put credit where the script comes from, the purpose of this organization was created to accommodate all the code we need, thank you to the people who participated and people who gave the start, if there are any, thank you very much 
+
+---------
+
+<h1 align="center">Hello Everbody! 🗿👌🏻</h1>
+<p align="center">
+  <img src="https://media.tenor.com/609sc-UxciwAAAAC/dancing-oshi-no-ko.gif" width=400 />
+</p>
+
+### My About
+- 👤 NeastooID
+- 💌 I am 1999 years old 
+- 👨‍💻 Not a group of sevuh but people who are copying and pasting 
+
+------
+
+### Special Thanks to
+
+[![Zevano](https://github.com/zevanoo.png?size=100)](https://github.com/zevanoo)
+[![Npc](https://github.com/NpnpicyAvailable.png?size=100)](https://github.com/NpnpicyAvailable)
+[![Glospot](https://github.com/glospotnew.png?size=100)](https://github.com/glospotnew)
+[![Furry](https://github.com/TukangM.png?size=100)](https://github.com/TukangM)
+[![Sevuh Yang Telang Hilang](https://github.com/Yusupkakuu.png?size=100)](https://github.com/Yusupkakuu)
